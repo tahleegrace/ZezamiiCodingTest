da4b8b41d4917d029c8552342e19ab00fb10b785
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2023-06-07 17:15:01 +1000

    Added support for adding new users.

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -8,21 +8,38 @@ import UsersList from './UsersList';
 let usersList = [];
 
 const AddUser = () => {
-	/*
+	const [username, setUsername] = useState('');
 
-	Add functions here. 
+	const usernameChanged = (event) => {
+		setUsername(event.target.value);
+	};
 
-	*/
+	const [age, setAge] = useState('');
+
+	const ageChanged = (event) => {
+		setAge(event.target.value);
+	};
+
+	const addUser = (event) => {
+		event.preventDefault(); // Stop the screen from being refreshed.
+
+		const newUser = {
+			username: username,
+			age: Number(age)
+		};
+
+		usersList.push(newUser);
+	};
 
 	return (
 		<div>
 			<Card className={classes.card}>
-				<form onSubmit={null} className={classes.form}>
+				<form onSubmit={addUser} className={classes.form}>
 					<label htmlFor="username">Username</label>
-					<input id="username" type="text" value={null} onChange={null} />
+					<input id="username" type="text" value={username} onChange={usernameChanged} required />
 					<label htmlFor="age">Age (Years)</label>
-					<input id="age" type="number" value={null} onChange={null} />
-					<Button type="submit" className={classes.submit}>
+					<input id="age" type="number" value={age} onChange={ageChanged} required />
+					<Button type="submit">
 						Add User
 					</Button>
 				</form>
