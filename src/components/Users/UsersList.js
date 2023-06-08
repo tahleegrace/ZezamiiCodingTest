e4475c7508391daa5a1d3bc810c12e50161c7009
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2023-06-08 13:03:26 +1000

    Added handling of ages where the age is 1.

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -4,10 +4,14 @@ import Card from '../UI/Card';
 import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
+	const getAgeText = (age) => {
+		return `${age} ${age > 1 ? 'years' : 'year'}`;
+	};
+
 	return (
 		<Card className={classes.users}>
 			<ul>
-				{props.users.map(user => (<li key={user.id}>{user.username} ({user.age} years old)</li>))}
+				{props.users.map(user => (<li key={user.id}>{user.username} ({getAgeText(user.age)} old)</li>))}
 			</ul>
 		</Card>
 	);
